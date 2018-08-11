caea1a1071530d68d0dec55268d5e30a33f914c7
kalpana167 <41733774+kalpana167@users.noreply.github.com>
2018-08-11 12:21:42 +0530

    Create prime.java

---
+++ program/prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i=11,temp;
+		temp=1;
+		if(i%1==0&&i%temp==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+
+	}
+}
